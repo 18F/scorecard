437badb8913e14841a41cbc808a5202e60089754
bwillman <benjamin.willman@gsa.gov>
2015-03-08 22:45:12 -0400

    Working for first two sections, need to add more content

--- scorecard.js
+++ scorecard.js
@@ -30,12 +30,12 @@ $(function(){
 })
 
 
-var tile_detail = "<div class='score-box black-border'>" + 
+var tile_detail = "<div class='score-box'>" + 
 					"<div style='height: 30px'>" +
-						"<div class='black-border pull-left'>Disagree</div>" + 
-						"<div class='black-border pull-right'>Agree</div>" +
+						"<div class='pull-left'>Disagree</div>" + 
+						"<div class='pull-right'>Agree</div>" +
 					"</div>" + 
-					"<div style='width: 220px' class='score-div black-border center-block'>" +
+					"<div style='width: 220px' class='score-div center-block'>" +
 
 						"<button type='button' class='btn btn-xs btn-default score-button' value='1'>1</button>" +
 						"<button type='button' class='btn btn-xs btn-default score-button' value='2'>2</button>" +
@@ -63,9 +63,12 @@ var setupTiles = function(){
 }
 
 var updateTileColor = function(pButton){
+
 	var this_tile = $(pButton).parentsUntil(".col-md-3",".tile");
 
 	if($(this_tile).attr("data-scoretype") == "1-is-bad") {
+
+
 		if ($(pButton).attr("value") == 1) {
 			clearTileColor(this_tile);
 			$(this_tile).addClass("score-bad");
@@ -88,11 +91,32 @@ var updateTileColor = function(pButton){
 			clearTileColor(this_tile);
 			$(this_tile).addClass("score-great");			
 		}
-
-		
 	}
+
 	else if ($(this_tile).attr("data-scoretype") == "1-is-good") {
 
+		if ($(pButton).attr("value") == 5) {
+			clearTileColor(this_tile);
+			$(this_tile).addClass("score-bad");
+		}
+		else if ($(pButton).attr("value") == 4) {
+			clearTileColor(this_tile);
+			$(this_tile).addClass("score-kinda-bad");
+
+		}
+		else if ($(pButton).attr("value") == 3) {
+			clearTileColor(this_tile);
+			$(this_tile).addClass("score-ok");			
+		}
+		else if ($(pButton).attr("value") == 2) {
+			clearTileColor(this_tile);
+			$(this_tile).addClass("score-kinda-good");			
+		}
+
+		else if ($(pButton).attr("value") == 1) {
+			clearTileColor(this_tile);
+			$(this_tile).addClass("score-great");			
+		}
 	}
 
 
